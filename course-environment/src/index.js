69e8f6474f798d0ff80d616140a9e5af0f822cd0
Josué Lustosa <josuelustosa.job@gmail.com>
2023-01-12 10:18:51 -0400

    :construction: JSX exercise

---
+++ course-environment/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import ExercicioJSX from "./ExercicioJSX";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ExercicioJSX />
+  </React.StrictMode>
+);
