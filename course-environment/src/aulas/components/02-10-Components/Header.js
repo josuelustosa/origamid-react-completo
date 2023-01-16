10386a64ca98837126040a8de1a7d940013a4096
Josué Lustosa <josuelustosa.job@gmail.com>
2023-01-16 08:06:22 -0400

    :construction: working with components

---
+++ course-environment/src/aulas/components/02-10-Components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header>Header aqui...</header>;
+};
+
+export default Header;
--- course-environment/src/index.js
+++ course-environment/src/index.js
@@ -5,12 +5,22 @@ import ReactDOM from "react-dom/client";
 import ExercicioJSX from "./aulas/ExercicioJSX";
 import JSXArrays from "./aulas/JSXArrays";
 import ExercicioJSXArrays from "./aulas/ExercicioJSXArrays";
+import Header from "./aulas/components/02-10-Components/Header";
+import Footer from "./aulas/components/02-10-Components/Footer";
+import Form from "./aulas/components/02-10-Components/Form/Form";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <ExercicioJSX /> */}
     {/* <JSXArrays /> */}
-    {<ExercicioJSXArrays />}
+    {/* {<ExercicioJSXArrays />} */}
+
+    {/* Components */}
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
   </React.StrictMode>
 );
