65460393e3663113f5dd9200a76eb9ce88d02fe7
Josué Lustosa <josuelustosa.job@gmail.com>
2023-01-16 09:49:12 -0400

    :construction: working with Rest e Spread

--- course-environment/src/index.js
+++ course-environment/src/index.js
@@ -9,6 +9,7 @@ import Header from "./aulas/components/02-10-Components/Header";
 import Footer from "./aulas/components/02-10-Components/Footer";
 import Form from "./aulas/components/02-10-Components/Form/Form";
 import Titulo from "./aulas/props/Titulo";
+import FormProps from "./aulas/props/FormProps";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -25,13 +26,16 @@ root.render(
     </> */}
 
     {/* Props */}
-    <>
+    {/* <>
       <Titulo texto="Meu Primeiro Título com Props" cor="red" />
       <Titulo texto="Meu Segundo Título com Props" cor="blue" />
       <Titulo texto="Meu Terceiro Título com Props e Children" cor="green">
         Isso é o Children do elemento h1 utilizando Props.
         <p>Passando um elemento "P" HTML por Children.</p>
       </Titulo>
+    </> */}
+    <>
+      <FormProps />
     </>
   </React.StrictMode>
 );
